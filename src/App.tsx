3d615ec7bd1617f9d67c268b0ff58b5a3e1e7982
johnkdunyo <johnkdunyo@gmail.com>
2022-06-25 18:45:04 +0000

    initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+let name: String;
+let age: string | number;
+// let classs : unknown;
+
+name = 'johns';
+age = 5;
+
+type Point = {
+  x: number,
+  y: number
+};
+
+const pt: Point = {x:5, y:6};
+console.log(pt)
+
+interface Point3d  {
+  x: number,
+  y: number
+}
+
+// extensiin insnt possible when you use type
+interface Point3d {
+  z: number;
+}
+
+const rect : Point3d ={x:5, y:7, z:3}
+console.log(rect)
+
+
+
+console.log(name, age)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello</p>
     </div>
   );
 }
