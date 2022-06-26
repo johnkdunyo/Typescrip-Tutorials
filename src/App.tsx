bdab7e975fbb6bed65d6c35221622fe17cb6b5f6
johnkdunyo <johnkdunyo@gmail.com>
2022-06-26 10:41:14 +0000

    detele, add , edit and create completed

--- src/App.tsx
+++ src/App.tsx
@@ -39,13 +39,13 @@ import {Todo } from './model'
 
 
 const App : React.FC =()  => {
-  const [todo, setTodo] = useState<string>(' ')
+  const [todo, setTodo] = useState<string>('')
   const [todos, setTodos] = useState<Todo []>([])
 
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault()
-    if(todo!==' '){
+    if(todo?.length > 0){
       setTodos([...todos, {id:Date.now(), todo, isDone: false}])
       setTodo('')
       console.log(todo)
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BiEditAlt} from 'react-icons/bi';
 import {MdDeleteForever, MdDownloadDone} from 'react-icons/md';
 
@@ -16,6 +16,14 @@ interface Props {
 
 const SingleTodo: React.FC<Props>= ({todo, todos, setTodos}) => {
 
+  const [editMode, setEditMode] = useState<boolean> (false)
+  const [editTodoText, setEditTodoText] = useState<string>(todo.todo)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(()=>{
+    inputRef.current?.focus()
+  }, [editMode]);
+
   const onClickDelete =(id:number) => {
     setTodos(todos=>todos.filter(todo=>todo.id!==id))
   }
@@ -24,23 +32,43 @@ const SingleTodo: React.FC<Props>= ({todo, todos, setTodos}) => {
     setTodos(
       todos.map(todo=>{
         if(todo.id===id){
-          todo.isDone = true
+          todo.isDone = !todo.isDone
         }
         return todo;
       })
     )
   }
 
+  const handleForm =(e: React.FormEvent, id:number) => {
+    e.preventDefault()
+    setTodos(
+      todos.map(todo=>{
+        if(todo.id===id){
+          todo.todo = editTodoText
+        }
+        return todo;
+      })
+    );
+    setEditMode(false)
+  }
+
+
   return (
     <form
     className='todos__single'
+    onSubmit={e=>handleForm(e, todo.id)}
     >
-      <span className="todos__single--text">{todo.todo}</span>
-      { todo.isDone.toString()}
+      {editMode ? <input ref={inputRef} value={editTodoText} onChange={e=>setEditTodoText(e.target.value)} className='todos__single--text'/> :
+      
+      todo.isDone ? <s className="todos__single--text">{todo.todo}</s> : <span className="todos__single--text">{todo.todo}</span>}
+     
      
 
       <div>
-        <span className="icon"> <BiEditAlt/></span>
+        <span className="icon" onClick={()=>{
+          setEditMode(prev=>!prev)
+
+        }}> <BiEditAlt/></span>
         <span className="icon" onClick={()=>onClickDelete(todo.id)}> <MdDeleteForever /></span>
         <span className="icon" onClick={()=>onClickDone(todo.id)}> <MdDownloadDone /> </span>
       </div>
